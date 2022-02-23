06b6e5187fa745c727a1b717739a098ce50d94cc
Rabus McCaleb <mccalebrabus@gmail.com>
2022-02-23 14:51:13 -0700

    Building submit functionality

--- Week 1.1 - HTML/1. Build a Form/index.js
+++ Week 1.1 - HTML/1. Build a Form/index.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
-
+    // This is the submit button that sends the form data off to it's propper destination via a post request 
     $('#submit').click(function (e) { 
         e.preventDefault();
-        console.log("Submit Button Click");
-        location.href = "/login/";
+        // console.log(`Submit Button Click`);
+        // location.href = "/login/";
     });
 
 });
+
+
